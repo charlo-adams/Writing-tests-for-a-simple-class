6cf4b54d15e4035da24f91bc743deb2c94c7fc36
charlo-adams <charlotteadams965@gmail.com>
2026-07-08 16:50:22 +0100

    final test

--- test/test_highValue.py
+++ test/test_highValue.py
@@ -6,6 +6,16 @@ def test_get_highest():
 
 
 def test_negative_numbers():
+    our_values = HighValue(-20, -43)
+    assert our_values.get_highest() == "First value is higher"
+
+def test_one_negative():
+    our_values = HighValue(-20, 43)
+    assert our_values.get_highest() == "Second value is higher"
+
+def test_zero_values():
+    our_values = HighValue(0, 12)
+    assert our_values.get_highest() == "Second value is higher"
 
 def test_get_first_highest():
     our_values = HighValue(88, 12)
@@ -24,3 +34,8 @@ def test_add_again():
     our_values = HighValue(68, 40)
     our_values.add(15, "second")
     assert our_values.value_second == 55
+
+def test_zero():
+    our_values = HighValue(0, 40)
+    our_values.add(15, "first")
+    assert our_values.value_first == 15
